153ebcea8faef3f858f875b02b181c1da3b7193b
GraceYu <librayuyueran@gmail.com>
2021-07-22 22:52:43 -0500

    finish the typscript crash course

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import AddToList from './component/AddToList';
+import List from './component/List';
+
+export interface IState {
+	people: {
+		name: string
+		age: number
+		url: string
+		note?: string
+	}[]
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [people, setPeople] = useState<IState["people"]>([
+		{
+			name: "LeBron James",
+			url: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/fruit-salad-horizontal-jpg-1522181219.jpg?crop=1xw:1xh;center,top&resize=768:*',
+			age: 36,
+			note: "Build encapsulated components that manage their own state."
+		}
+	])
+
+	people.map(p => p.name)
+
+	return (
+		<div className="App">
+			<h1>People Invited to my Party</h1>
+			<List people={people}/>
+			<AddToList people={people} setPeople={setPeople}/>
+		</div>
+	);
 }
 
 export default App;
